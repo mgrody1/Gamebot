913d25896cf82c81c688ea9d8fc4d7e69ed18596
mgrody1 <mgrody1@umbc.edu>
2025-11-05 23:42:21 -0500

    pre-commit fixes

--- dbt/macros/generate_surrogate_key.sql
+++ dbt/macros/generate_surrogate_key.sql
@@ -1,4 +1,3 @@
-# This macro replaces dbt_utils.generate_surrogate_key with native PostgreSQL
 {% macro generate_surrogate_key(columns) %}
     md5(concat({% for column in columns %}{{ column }}{% if not loop.last %}, '|', {% endif %}{% endfor %}))
 {% endmacro %}
